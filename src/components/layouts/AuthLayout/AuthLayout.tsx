3acf278f826bfaf4c98317484340ae999d1921c7
KonekkoNekko <nabiel.setiawan@gmail.com>
2025-03-21 03:55:03 +0700

    feat: login

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -5,15 +5,16 @@ interface PropTypes {
   children: ReactNode;
   title?: string;
 }
+
 const AuthLayout = (props: PropTypes) => {
   const { children, title } = props;
   return (
-    <Fragment>
+    <div className="lg:py flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10">
       <PageHead title={title} />
       <section className="max-w-screen-3xl 3xl:container flex p-8">
         {children}
       </section>
-    </Fragment>
+    </div>
   );
 };
 
